7a23b793763c3561a82b8ea032c2828773fd87c5
Adam Cankaya <adamcankaya@gmail.com>
2015-05-11 20:07:46 -0400

    Added forms to collect new Organization and Item data from user

---
+++ public_html/addCat2.php
@@ -0,0 +1,21 @@
+<?php 
+		ini_set('display_errors', 'On');
+		include 'library.php'; 
+?>
+
+<?php
+	$mysqli = new mysqli($serverAddress, $user, $password, $database);
+	$mysqli->select_db($database) or die ("Oops! DB not connected"); // select the database
+
+    $categoryId = "NULL";
+    $categoryName = $_POST['catName'];
+
+    echo "{'categoryId':'" . $categoryId . "', 'categoryName':'" . $categoryName. "'}";
+
+#	$query = "INSERT INTO Category VALUES(NULL,'$catName')";
+#	$result = $mysqli->query($query);
+#	if($result)
+#	    echo "<br>Success!";
+#	else
+#	    echo "<br>Fail!";
+?>
---
+++ public_html/addItem2.php
@@ -0,0 +1,22 @@
+<?php 
+		ini_set('display_errors', 'On');
+		include 'library.php'; 
+?>
+
+<?php
+	$mysqli = new mysqli($serverAddress, $user, $password, $database);
+	$mysqli->select_db($database) or die ("Oops! DB not connected"); // select the database
+
+    $itemId = "NULL";
+    $itemName = $_POST['itemName'];
+    $categoryRef = $_POST['catID'];
+
+    echo "{'itemId':'" . $itemId . "', 'itemName':'" . $itemName . "', 'categoryRef':'" . $categoryRef . "'}";
+
+#	$query = "INSERT INTO Category VALUES(NULL,'$catName')";
+#	$result = $mysqli->query($query);
+#	if($result)
+#	    echo "<br>Success!";
+#	else
+#	    echo "<br>Fail!";
+?>
