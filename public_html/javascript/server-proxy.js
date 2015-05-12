862502e0e673df299864fa14ead84c621f984faa
Donald Subert <helo478@gmail.com>
2015-05-11 17:13:07 -0700

    added insert to the server proxy

--- public_html/javascript/server-proxy.js
+++ public_html/javascript/server-proxy.js
@@ -7,17 +7,51 @@
  *
  * handles transactions with the server
  *
- * @constructor
+ * @constructor resolves the constant SERVER_URL based on local environment
  * @depends jQuery
  */
 function ServerProxy() {
 
-    const SERVER_URL =
-        "http://web.engr.oregonstate.edu/~subertd/ReuseAndRepairServer/public_html/data.php";
+    const CONFIG_PATH = "javascript/local-config.json";
+
+    var serverUrl;
+
+    $.ajax({
+        url: CONFIG_PATH,
+        dataType: 'json',
+        async: false,
+        success: function(data) {
+            serverUrl = data['serverUrl'];
+        },
+        error: function(error) {
+            console.log("Error getting local-config.json", error);
+        }
+    });
 
     return {
         syncDatabase: function(callback) {
-            $
+            $.ajax({
+                url: serverUrl,
+                method: "get",
+                success: callback
+            });
+        },
+        insert: function(type, organization, callback) {
+            $.ajax({
+                url: serverUrl,
+                method: "post",
+                headers: {
+                    "action" : type,
+                    "contentType": "application/json"
+                },
+                data: organization,
+                dataType: 'json',
+
+                success: callback,
+                error: function(error) {
+                    console.log(error);
+                }
+            })
         }
     }
 }
