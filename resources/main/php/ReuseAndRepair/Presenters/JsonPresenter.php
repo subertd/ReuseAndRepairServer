b7f5c38a84e007df3ee7d79383ac66650368ce46
Donald <subertd@onid.oregonstate.edu>
2015-05-18 17:20:30 -0700

    Fixed issues #4 and #5

--- resources/main/php/ReuseAndRepair/Presenters/JsonPresenter.php
+++ resources/main/php/ReuseAndRepair/Presenters/JsonPresenter.php
@@ -12,8 +12,33 @@ namespace ReuseAndRepair\Presenters;
 class JsonPresenter implements Presenter {
 
     public function presentResponse(array $response) {
+
+        //print_r($response);
+
+        $utf8Response = $this->utf8ize($response);
+
+        //die("utf8Response$utf8Response'");
+
         header("content-type:application/json");
-        echo json_encode($response);
+        echo json_encode($utf8Response);
+    }
+
+    /**
+     * Recursively changed the encoding of all string proerties and sub-properties
+     * to utf8
+     *
+     * @citation the following method uses code from this forum thread
+     * http://stackoverflow.com/questions/19361282/why-would-json-encode-returns-an-empty-string
+     */
+    private function utf8ize($d) {
+        if (is_array($d)) {
+            foreach ($d as $k => $v) {
+                $d[$k] = $this->utf8ize($v);
+            }
+        } else if (is_string ($d)) {
+            return utf8_encode($d);
+        }
+        return $d;
     }
 
     public function presentException(\Exception $e) {
